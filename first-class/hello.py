0490fa12bbe61920eb8d2de58e3733060b7de1fe
aoliveira94 <adriano.muniz@outlook.com.br>
2023-02-22 22:13:23 -0300

    multi languages

--- first-class/hello.py
+++ first-class/hello.py
@@ -1,1 +1,23 @@
-print('hello, world!'.upper())
+#!/usr/bin/env python3
+
+__version__ = "0.0.1"
+__author__  = "Adriano Santos"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+
+print(msg)
